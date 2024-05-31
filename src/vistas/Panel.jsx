f49491da0f6ee1615d6d6a1343145840061da133
DavidValdezateGarciaLlefia <120660026+DavidValdezateGarciaLlefia@users.noreply.github.com>
2024-05-31 17:11:49 +0200

    Add files via upload

---
+++ src/vistas/Panel.jsx
@@ -0,0 +1,43 @@
+
+import { GlobalContext } from "../context/GlobalContext";
+import { TablaPendientes } from "../componentes/TablaPendientes";
+import { TablaResueltos } from "../componentes/TablaResueltos";
+import { useContext } from "react";
+
+export function Panel() {
+    const { datos } = useContext(GlobalContext);
+
+    return (
+        <div>
+            <h1>Administración de incidencias</h1>
+            <h2 className="mt-5">Tickets pendientes</h2>
+            {datos.ticketsPendientes.map(ticket => (
+                <TablaPendientes
+                    key={ticket.codigo}
+                    codigo={ticket.codigo}
+                    fecha={ticket.fecha}
+                    aula={ticket.aula}
+                    grupo={ticket.grupo}
+                    ordenador={ticket.ordenador}
+                    descripcion={ticket.descripcion}
+                    alumno={ticket.alumno}
+                />
+            ))}
+            
+            <h2 className="mt-5">Tickets resueltos</h2>
+            {datos.ticketsResueltos.map(ticket => (
+                <TablaResueltos
+                    key={ticket.codigo}
+                    codigo={ticket.codigo}
+                    fecha={ticket.fecha}
+                    fecha_resuelto={ticket.fecha_resuelto}
+                    aula={ticket.aula}
+                    grupo={ticket.grupo}
+                    ordenador={ticket.ordenador}
+                    descripcion={ticket.descripcion}
+                    alumno={ticket.alumno}
+                />
+            ))}
+        </div>
+    );
+}
