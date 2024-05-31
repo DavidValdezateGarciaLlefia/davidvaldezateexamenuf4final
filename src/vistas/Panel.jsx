4f1f23db11cb141346224c6e805e37a13df8e8fc
DavidValdezateGarciaLlefia <valdezategarciadavid@fpllefia.com>
2024-05-31 18:56:05 +0200

    commit

--- src/vistas/Panel.jsx
+++ src/vistas/Panel.jsx
@@ -6,38 +6,41 @@ import { useContext } from "react";
 export function Panel() {
     const { datos } = useContext(GlobalContext);
 
+    const ticketsPendientesOrdenados = datos.ticketsPendientes.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+    const ticketsResueltosOrdenados = datos.ticketsResueltos.sort((a, b) => new Date(a.fecha) - new Date(b.fecha_resuelto));
+
     return (
         <div>
             <h1>Administración de incidencias</h1>
             <h2 className="mt-5">Tickets pendientes</h2>
             <table className="table mt-4">
-            <thead>
-                <tr>
-                    <th>Código</th>
-                    <th>Fecha</th>
-                    <th>Aula</th>
-                    <th>Grupo</th>
-                    <th>Ordenador</th>
-                    <th>Descripción</th>
-                    <th>Alumno</th>
-                    <th>Resolver</th>
-                    <th>Comentarios</th>
-                    <th>Eliminar</th>
-                </tr>
-            </thead>
-            {datos.ticketsPendientes.map(ticket => (
-                <TablaPendientes
-                    key={ticket.id}
-                    id={ticket.id}
-                    codigo={ticket.codigo}
-                    fecha={ticket.fecha}
-                    aula={ticket.aula}
-                    grupo={ticket.grupo}
-                    ordenador={ticket.ordenador}
-                    descripcion={ticket.descripcion}
-                    alumno={ticket.alumno}
-                />
-            ))}
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Fecha</th>
+                        <th>Aula</th>
+                        <th>Grupo</th>
+                        <th>Ordenador</th>
+                        <th>Descripción</th>
+                        <th>Alumno</th>
+                        <th>Resolver</th>
+                        <th>Comentarios</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                {ticketsPendientesOrdenados.map(ticket => (
+                    <TablaPendientes
+                        key={ticket.id}
+                        id={ticket.id}
+                        codigo={ticket.codigo}
+                        fecha={ticket.fecha}
+                        aula={ticket.aula}
+                        grupo={ticket.grupo}
+                        ordenador={ticket.ordenador}
+                        descripcion={ticket.descripcion}
+                        alumno={ticket.alumno}
+                    />
+                ))}
             </table>
             <h2 className="mt-5">Tickets resueltos</h2>
             <table className="table mt-4">
@@ -55,20 +58,20 @@ export function Panel() {
                         <th>Eliminar</th>
                     </tr>
                 </thead>
-            {datos.ticketsResueltos.map(ticket => (
-                <TablaResueltos
-                    key={ticket.id}
-                    id={ticket.id}
-                    codigo={ticket.codigo}
-                    fecha={ticket.fecha}
-                    fecharesuelto={ticket.fecha_resuelto}
-                    aula={ticket.aula}
-                    grupo={ticket.grupo}
-                    ordenador={ticket.ordenador}
-                    descripcion={ticket.descripcion}
-                    alumno={ticket.alumno}
-                />
-            ))}
+                {ticketsResueltosOrdenados.map(ticket => (
+                    <TablaResueltos
+                        key={ticket.id}
+                        id={ticket.id}
+                        codigo={ticket.codigo}
+                        fecha={ticket.fecha}
+                        fecharesuelto={ticket.fecha_resuelto}
+                        aula={ticket.aula}
+                        grupo={ticket.grupo}
+                        ordenador={ticket.ordenador}
+                        descripcion={ticket.descripcion}
+                        alumno={ticket.alumno}
+                    />
+                ))}
             </table>
         </div>
     );
